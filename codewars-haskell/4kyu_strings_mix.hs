c655226c18b5c9b1ddf61cbc25a38a67d62704a2
dcfxyz <dcf_xyz@outlook.com>
2016-11-09 23:59:02 +0800

    日常刷题 16-11-9

--- codewars-haskell/4kyu_strings_mix.hs
+++ codewars-haskell/4kyu_strings_mix.hs
@@ -1,24 +1,37 @@
 import Data.Char
+import Data.List
 
-traval :: String -> [Int] -> [Int]
-traval [] ys = ys
-traval (x:xs) ys = traval xs ys'
-  where ind = (ord x) - (ord 'a')
-        ys' = take ind ys ++ (ys!!(ind+1)+1 : drop (ind+1) ys)
+getCount :: String -> [(Char,Int)]
+getCount xs = zip ['a'..'z'] $ map (\s -> length s - 1) $ group.sort $ ['a'..'z'] ++ (filter isLower xs)
 
-myShow :: [Int] -> [Int] -> [String]
-myShow [] [] = [[]]
-myShow (x:xs) (y:ys)
-  | x == y = (if x==0 then "" else '/':'=':':':[chx|_<-[1..x]]) : myShow xs ys
-  | x >  y = ('/':'1':':':[chx|_<-[1..x]]) : myShow xs ys
-  | x <  y = ('/':'2':':':[chy|_<-[1..y]]) : myShow xs ys
-  where chx = chr (ord 'a' + x)
-        chy = chr (ord 'a' + y)
+toShow :: (Char,Int,Int) -> String
+toShow (ch,n,t)
+  | t == 1 = "/1:"++xs
+  | t == 2 = "/2:"++xs
+  | t == 3 = "/=:"++xs
+  where xs = take n $ repeat ch
+
+mergeInfo :: (Char,Int) -> (Char,Int) -> (Char,Int,Int)
+mergeInfo (ch1,n1) (ch2,n2)
+  | max n1 n2 <= 1 = (ch1,0,0)
+  | n1 == n2 = (ch1,n1,3)
+  | n1 >  n2 = (ch1,n1,1)
+  | n1 <  n2 = (ch2,n2,2)
+
+cmp :: (Char,Int,Int) -> (Char,Int,Int) -> Ordering
+cmp (ch1,n1,t1) (ch2,n2,t2)
+  | n1 > n2 = LT
+  | n1 < n2 = GT
+  | t1 < t2 = LT
+  | t1 > t2 = GT
+  | ch1 < ch2 = LT
+  | ch1 > ch2 = GT
+  | otherwise = EQ
 
 mix :: String -> String -> String
-mix s1 s2 = tail $ concat $ myShow xs1 xs2
-  where xs1 = traval (map toLower s1) xs
-        xs2 = traval (map toLower s2) xs
-        xs  = take 26 $ repeat 0
+mix s1 s2 = if length ans == 0 then ans else tail ans
+  where xs1 = getCount s1
+        xs2 = getCount s2
+        ans = concat $ map toShow $ sortBy cmp $ filter (\(_,n,_)->n/=0) $ zipWith mergeInfo xs1 xs2
 
 
